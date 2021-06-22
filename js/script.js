b8286371d47e06c6a01b85e6b3bdf01ebe4b7e5e
Lucas Sales <bortolinilucas20@gmail.com>
2021-06-22 20:24:13 -0300

    Add modal "order completed"

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function abrirModal() {
+  let modal = document.querySelector('.modal')
+  modal.style.visibility = 'visible';
+}
+
+function fecharModal() {
+  let modal = document.querySelector('.modal')
+  modal.style.visibility = 'hidden';
+}
