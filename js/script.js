8fd31400c5a1272af71a82e616416320e211660f
Lucas Sales <bortolinilucas20@gmail.com>
2021-06-22 21:47:08 -0300

    include orders page and modal on payment method

--- js/script.js
+++ js/script.js
@@ -7,3 +7,41 @@ function fecharModal() {
   let modal = document.querySelector('.modal')
   modal.style.visibility = 'hidden';
 }
+
+  console.log($('.minus-btn'))
+
+$('.minus-btn').on('click', function (e) {
+  console.log('oi')
+  e.preventDefault();
+  var $this = $(this);
+  var $input = $this.closest('div').find('input');
+  var value = parseInt($input.val());
+
+  if (value > 1) {
+    value = value - 1;
+  } else {
+    value = 0;
+  }
+
+  $input.val(value);
+
+});
+
+$('.plus-btn').on('click', function (e) {
+  e.preventDefault();
+  var $this = $(this);
+  var $input = $this.closest('div').find('input');
+  var value = parseInt($input.val());
+
+  if (value < 100) {
+    value = value + 1;
+  } else {
+    value = 100;
+  }
+
+  $input.val(value);
+});
+
+$('.like-btn').on('click', function () {
+  $(this).toggleClass('is-active');
+});
